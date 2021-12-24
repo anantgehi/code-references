373faff20b04464152399ebcbf7a4919aef2cdc1
anantgehi <89633206+anantgehi@users.noreply.github.com>
2021-12-24 13:33:38 +0530

    Update PrintHashCodes.java

--- Java/code/day14.1/src/tester/PrintHashCodes.java
+++ Java/code/day14.1/src/tester/PrintHashCodes.java
@@ -3,6 +3,9 @@ package tester;
 public class PrintHashCodes {
 
 	public static void main(String[] args) {
+		
+		//All these strings will produce same hashcode hence we also use equals method with hashing
+		
 		//BBBB AaBB AaAa BBAa
 		System.out.println("BBBB".hashCode());
 		System.out.println("AaBB".hashCode());
