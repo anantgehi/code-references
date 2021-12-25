2fda624b8bf629344a8aaa43039eee9399878656
anantgehi <89633206+anantgehi@users.noreply.github.com>
2021-12-25 23:23:02 +0530

    Update BankingApp.java

--- Java/code/day15.1/src/tester/BankingApp.java
+++ Java/code/day15.1/src/tester/BankingApp.java
@@ -29,10 +29,11 @@ public class BankingApp {
 				
 				System.out.println("Options 1. Create A/C 2. Display 3. Get A/C Summary "
 						+ "4. Transfer Funds 5. Close A/C 6. Display all account details of specific account type "
-						+ "7. Remove all loan accounts10.Exit");
+						+ "7.Display all account details of having entered minimum balance 8. Remove all loan accounts10.Exit");
 				try {
 					switch (sc.nextInt()) {
-					case 1:
+					
+						case 1:
 						System.out.println("Enter acct no");
 						int acctNo = sc.nextInt();
 						
@@ -56,22 +57,33 @@ public class BankingApp {
 						System.out.println(findByAccountNo(sc.nextInt(), accounts));
 
 						break;
+							
 					case 4: // Funds Transfer
+						
 						System.out.println("Enter src dest acct nos n amount to transfer");
 						BankAccount src = findByAccountNo(sc.nextInt(), accounts);
 						BankAccount dest = findByAccountNo(sc.nextInt(), accounts);
+
 						// => src n dest a/c exist
+						
 						src.transferFunds(dest, sc.nextDouble());
 						System.out.println("funds transferred....");
 						break;
+							
 					case 5: // close a/c
+						
 						System.out.println("Enter a/c no to close a/c");
+						
 						src = accounts.remove(sc.nextInt());
+						
 						if (src == null)
 							throw new BankingException("A/C Closing Failed : Invalid a/c no!!!!!");
+						
 						System.out.println("Closed a/c details " + src);
 						break;
-					case 6: // display based upon value based criteria
+							
+					case 6: // display based upon value based criteria - account type
+						
 						System.out.println("Enter a/c type");
 						AccountType type = valueOf(sc.next().toUpperCase());
 						System.out.println("A/cs of type " + type);
@@ -80,20 +92,33 @@ public class BankingApp {
 								System.out.println(a);
 
 						break;
+							
+					case 7: // display based upon value based criteria - min balance
+						
+						System.out.println("Enter amount");
+						double bal = sc.nextDouble();
+						System.out.println("A/cs having minimum bal" + bal);
+						for (BankAccount a : accounts.values())
+							if (a.getBalance() >= bal)
+								System.out.println(a);
 
-					case 7: // remove loan accounts
+						break;
+
+					case 8: // remove loan accounts
+						
 						Iterator<BankAccount> itr = accounts.values().iterator();
+						//Collection<BankAccount> collection=accounts.values();
 						System.out.println("removing loan accounts");
-//						for(BankAccount a : collection) //implicit itr : raises ConcurrentModificationExc
-//							if(a.getAcType()==LOAN)
-//							collection.remove(a);
+					//	for(BankAccount a : collection) //for each is implicit itr : raises ConcurrentModificationExc
+					//	if(a.getAcType()==LOAN)
+					//	collection.remove(a);//if we try to use remove other than iterators remove method it throws concurrentmodexc
 						while (itr.hasNext()) {
 							if (itr.next().getAcType() == LOAN)
-								itr.remove();
+								itr.remove();//using this we are removing reference from collection as well as entry from map
 
 						}
-						// Think it over : ConcurrentModoficationException , When u use
-						// values/keySet/entrySet , Colleciton is backed by the Map n vice versa.
+					// Think it over : ConcurrentModoficationException , When u use
+					// values/keySet/entrySet , Colleciton is backed by the Map n vice versa.Any changes is collection will reflect on map
 
 						break;
 
@@ -101,6 +126,7 @@ public class BankingApp {
 						exit = true;
 						break;
 					}
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
